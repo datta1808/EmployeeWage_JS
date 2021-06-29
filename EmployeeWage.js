42cb396ef0d8ef4fe83332effb04ac29b5b1b9d0
datta1808 <bagaledatta18@gmail.com>
2021-06-29 21:55:40 +0530

    ADDED : Functionality to Calculate Wage According to Condition

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,4 +1,4 @@
-// UC 4 - Calculate Wages for a Month 
+// UC 5 - Calculating Wages till Number of Working Days or Total Working Hours Per Month is Reached
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -6,6 +6,7 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20; 
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 100;
 
 function getWorkingHours(empCheck) {
     switch(empCheck){
@@ -18,11 +19,16 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let empHrs = 0;
-for (let day=0; day<NUM_OF_WORKING_DAYS; day++) {
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
 }
 
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Employee Hours = " + empHrs + " Total Employee Wage = " + empWage)
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+
+console.log("Total working days: " + totalWorkingDays);
+console.log("Total employee hours = " + totalEmpHrs+" Total employee wage = " + empWage);
